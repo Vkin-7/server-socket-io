4ea4676304e80e02418f98a190a477e5ad0f4a9e
Victor Hugo <54605503+Vkin-7@users.noreply.github.com>
2020-08-29 19:04:06 -0300

    teste

--- server.js
+++ server.js
@@ -1,3 +1,86 @@
+// const express = require('express')
+// const app = express()
+
+// const http = require('http').Server(app)
+// const io = require('socket.io')(http)
+
+// const routes = require("./src/routes");
+// const cors = require('cors')
+
+
+// const port = process.env.PORT || 3333;
+
+// app.use(cors())
+// app.use(express.json())
+// app.use(routes)
+
+    
+//     /*let interval;
+    
+//     io.on("connection", (socket) => {
+//       console.log("New client connected");
+//       if (interval) {
+//         clearInterval(interval);
+//       }
+//       interval = setInterval(() => getApiAndEmit(socket), 1000);
+//       socket.on("disconnect", () => {
+//         console.log("Client disconnected");
+//         clearInterval(interval);
+//       });
+//     });
+    
+//     const getApiAndEmit = socket => {
+//         const response = new Date();
+//         // Emitting a new message. Will be consumed by the client
+//         socket.emit("FromAPI", response);
+//       };*/
+
+
+// http.listen(port, () => console.log(`Listening on port ${port}`))
+
+// let connectedPeers = new Map()
+
+// io.on('connection', socket => {
+//   console.log(socket.id)
+
+//   socket.emit('connection-success', {success: socket.id})
+
+//   connectedPeers.set(socket.id, socket)
+
+//   socket.on('disconnect', () => {
+//     console.log('disconnected')
+//     connectedPeers.delete(socket.id)
+//   })
+
+//   socket.on('offerOrAnswer', data => {
+//     //send to other peer(s) if any
+//     for(const [socketID, socket] of connectedPeers.entries()){
+//       //dont send to self
+//       if(socketID !== data.socketID){
+//         console.log(socketID, data.payload.type)
+//         socket.emit('offerOrAnswer', data.payload)
+//       }
+//     }
+//   })
+
+//   socket.on('candidate', data => {
+//     //send candidates to the other peer(s) if any
+//     for(const [socketID, socket] of connectedPeers.entries()){
+//       //dont send to self
+//       if(socketID !== data.socketID){
+//         console.log(socketID, data.payload)
+//         socket.emit('candidate', data.payload)
+//       } 
+//     }
+//   })
+// })
+// const express = require("express");
+// const http = require("http");
+// const app = express();
+// const server = http.createServer(app);
+// const socket = require("socket.io");
+// const io = socket(server);
+
 const express = require('express')
 const app = express()
 
@@ -7,70 +90,31 @@ const io = require('socket.io')(http)
 const routes = require("./src/routes");
 const cors = require('cors')
 
-
 const port = process.env.PORT || 3333;
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-    
-    /*let interval;
-    
-    io.on("connection", (socket) => {
-      console.log("New client connected");
-      if (interval) {
-        clearInterval(interval);
-      }
-      interval = setInterval(() => getApiAndEmit(socket), 1000);
-      socket.on("disconnect", () => {
-        console.log("Client disconnected");
-        clearInterval(interval);
-      });
-    });
-    
-    const getApiAndEmit = socket => {
-        const response = new Date();
-        // Emitting a new message. Will be consumed by the client
-        socket.emit("FromAPI", response);
-      };*/
+http.listen(port, () => console.log(`server is running on port ${port}`));
 
-
-http.listen(port, () => console.log(`Listening on port ${port}`))
-
-let connectedPeers = new Map()
-
+const users = {};
+ 
 io.on('connection', socket => {
-  console.log(socket.id)
-
-  socket.emit('connection-success', {success: socket.id})
-
-  connectedPeers.set(socket.id, socket)
-
-  socket.on('disconnect', () => {
-    console.log('disconnected')
-    connectedPeers.delete(socket.id)
-  })
-
-  socket.on('offerOrAnswer', data => {
-    //send to other peer(s) if any
-    for(const [socketID, socket] of connectedPeers.entries()){
-      //dont send to self
-      if(socketID !== data.socketID){
-        console.log(socketID, data.payload.type)
-        socket.emit('offerOrAnswer', data.payload)
-      }
+    if (!users[socket.id]) { 
+        users[socket.id] = socket.id;
     }
-  })
+    socket.emit("yourID", socket.id);
+    io.sockets.emit("allUsers", users);
+    socket.on('disconnect', () => {
+        delete users[socket.id];
+    })
 
-  socket.on('candidate', data => {
-    //send candidates to the other peer(s) if any
-    for(const [socketID, socket] of connectedPeers.entries()){
-      //dont send to self
-      if(socketID !== data.socketID){
-        console.log(socketID, data.payload)
-        socket.emit('candidate', data.payload)
-      } 
-    }
-  })
-})
+    socket.on("callUser", (data) => {
+        io.to(data.userToCall).emit('hey', {signal: data.signalData, from: data.from});
+    })
+
+    socket.on("acceptCall", (data) => {
+        io.to(data.to).emit('callAccepted', data.signal);
+    })
+});
